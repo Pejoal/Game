323ac63ce98cb4730320a62ca67ddc9cbc0c2211
pejoal <pejoal.official@gmail.com>
2024-05-29 16:47:03 +0200

    Ensure Each Player get unique cards

--- app/Http/Controllers/LobbyController.php
+++ app/Http/Controllers/LobbyController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Events\LobbyMessageSent;
 use App\Events\LobbyStarted;
+use App\Models\Card;
 use App\Models\Lobby;
+use App\Models\LobbyCard;
 use App\Models\Story;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -31,31 +33,49 @@ class LobbyController extends Controller {
   }
 
   public function start(Request $request, Lobby $lobby, Story $story) {
-    $cardGroups = $story->cardGroups()->with('cards')->get();
-    $cards = [];
+    if (auth()->id() == $lobby->host_id && !LobbyCard::where('lobby_id', $lobby->id)->exists()) {
+      $cardGroups = $story->cardGroups()->with('cards')->get();
+      $cards = [];
 
-    foreach ($cardGroups as $group) {
-      foreach ($group->cards as $card) {
-        $cards[] = $card;
+      foreach ($cardGroups as $group) {
+        foreach ($group->cards as $card) {
+          $cards[] = $card;
+        }
+      }
+
+      shuffle($cards);
+
+      $users = $lobby->users;
+      $userCount = count($users);
+      $cardsPerUser = floor(count($cards) / $userCount);
+
+      // Distribute the cards equally among users
+      $userCards = [];
+      for ($i = 0; $i < $userCount; $i++) {
+        $userCards[$users[$i]->id] = array_slice($cards, $i * $cardsPerUser, $cardsPerUser);
+      }
+
+      // Handle remaining cards if there are any
+      $remainingCards = array_slice($cards, $userCount * $cardsPerUser);
+      foreach ($remainingCards as $index => $card) {
+        $userCards[$users[$index % $userCount]->id][] = $card;
+      }
+      foreach ($userCards as $user_id => $cards) {
+        foreach ($cards as $key => $card) {
+          LobbyCard::create([
+            'user_id' => $user_id,
+            'card_id' => $card->id,
+            'lobby_id' => $lobby->id,
+          ]);
+        }
       }
     }
 
-    shuffle($cards);
-
-    $users = $lobby->users;
-    $userCount = count($users);
-    $cardsPerUser = floor(count($cards) / $userCount);
-
-    // Distribute the cards equally among users
+    $_userCards = LobbyCard::where('lobby_id', $lobby->id)->get();
     $userCards = [];
-    for ($i = 0; $i < $userCount; $i++) {
-      $userCards[$users[$i]->id] = array_slice($cards, $i * $cardsPerUser, $cardsPerUser);
-    }
 
-    // Handle remaining cards if there are any
-    $remainingCards = array_slice($cards, $userCount * $cardsPerUser);
-    foreach ($remainingCards as $index => $card) {
-      $userCards[$users[$index % $userCount]->id][] = $card;
+    foreach ($_userCards as $lobbyCard) {
+      $userCards[$lobbyCard->user_id][] = Card::find($lobbyCard->card_id);
     }
 
     $user = $request->user();
